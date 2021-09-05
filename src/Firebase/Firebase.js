cab2277e122416130609bfe5f7f7dd5eb6e8b32c
argon <premsagark.ug18.ee@nitp.ac.in>
2021-09-05 11:42:14 +0530

    firebase config changed

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -5,16 +5,26 @@ import "firebase/firestore";
 import "firebase/storage";
 
 // Firebase Config
-const firebaseConfig = {
-    apiKey: "AIzaSyBIrIcDMj0X9oM7Xd8KOJ8tpny2SyQDbeA",
-    authDomain: "wetalk-argon17.firebaseapp.com",
-    databaseURL: "https://wetalk-argon17-default-rtdb.firebaseio.com",
-    projectId: "wetalk-argon17",
-    storageBucket: "wetalk-argon17.appspot.com",
-    messagingSenderId: "473443794549",
-    appId: "1:473443794549:web:aa9948674228b39bce823c"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBIrIcDMj0X9oM7Xd8KOJ8tpny2SyQDbeA",
+//     authDomain: "wetalk-argon17.firebaseapp.com",
+//     databaseURL: "https://wetalk-argon17-default-rtdb.firebaseio.com",
+//     projectId: "wetalk-argon17",
+//     storageBucket: "wetalk-argon17.appspot.com",
+//     messagingSenderId: "473443794549",
+//     appId: "1:473443794549:web:aa9948674228b39bce823c"
+// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBZfDb1p9Yp-C1wXP7uuCRA_u8jOAfWljQ",
+    authDomain: "wetalk-chatapp.firebaseapp.com",
+    projectId: "wetalk-chatapp",
+    storageBucket: "wetalk-chatapp.appspot.com",
+    messagingSenderId: "414296379358",
+    appId: "1:414296379358:web:550cf82666fc334cf5d74f",
+    measurementId: "G-1MLMS9RXCC"
+  };
 
 // initialize the app
 const firebaseApp = firebase.initializeApp(firebaseConfig);
